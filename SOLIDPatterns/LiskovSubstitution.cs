99c74d84405b785b315809c59bcd27a5086be1fb
Himanshu Panwar <Himanshu_Panwar@Dell.com>
2023-02-07 20:30:49 +0530

    Add project files.

---
+++ SOLIDPatterns/LiskovSubstitution.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLIDPatterns
+{
+    public class Rectangle
+    {
+
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+
+        public override int Width // nasty side effects
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public override int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+    }
+
+
+    internal class LiskovSubstitution
+    {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2, 3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            // should be able to substitute a base type for a subtype
+            /*Square*/
+            Rectangle sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+        }
+
+    }
+}
---
+++ SOLIDPatterns/SingleResponsibility.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SOLIDPatterns
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+        //breaks single responsibility principle 
+        public void Save(string filename, bool overwrite = false)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+    }
+
+    //handles the responsibility of persisting objects
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false) {
+            if(overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, journal.ToString());
+            }
+        }
+    }
+    class SingleResponsibility
+    {
+        static void MainSP(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("I am happy");
+            j.AddEntry("I am himanshu");
+            j.AddEntry("I am Akshita");
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"C:\temp\journal.txt";
+            p.SaveToFile(j, filename);
+            Console.WriteLine(filename);
+        }
+    }
+}
